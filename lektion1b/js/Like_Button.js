ee130d78c772dd2aaffb474ec87091f34d16e15d
aichualym <aichurok840@gmail.com>
2022-03-22 21:13:46 +0100

    Aufgabe 1b ist erledigt

--- lektion1b/js/Like_Button.js
+++ lektion1b/js/Like_Button.js
@@ -1,8 +1,8 @@
 
 
 /*ToDo: Füge hinter 'extends' den fehlenden Ausdruck ein und entferne die Kommentar-zeichen*/
-/*
-class Like_Button extends {
+
+class Like_Button extends React.Component {
     constructor(props) {
         super(props);
         this.state = { liked: false };
@@ -11,13 +11,12 @@ class Like_Button extends {
     render = () => {
         if (this.state.liked) {
             //ToDo: Füge hier ein Rückgabewert hinzu, der anstelle des "Gefällt Mir"-Buttons angezeigt werden soll
-
+        return ("Dir gefällt das")
         }
         return (
             //ToDo: Füge hier HTML-Code ein, der einen "Gefällt Mir"-Button beinhaltet und ein onClick-Even verarbeitet
-
+    <div><button onClick={() => this.setState({ liked: true }) }>Gefällt mir!</button> </div>
 
         );
     }
 }
-*/
